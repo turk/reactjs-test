c52f7fc4b72e59ff380958186d3e7c9fc99ab64b
Suleyman Yilmaz <sy@suleymanyilmaz.org>
2020-02-09 17:02:46 +0200

    Adjust sidebar

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -1,40 +1,83 @@
 import React, {Component, Fragment} from 'react';
-import Item from '../sidebar/Item';
-class Sidebar extends Component {
+import {Link} from "react-router-dom";
+import classNames from 'classnames';
 
+class Sidebar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isOpen: false,
+            isOpen: [],
+            items: [],
         };
 
+        this.handleToggle = this.handleToggle.bind(this);
+    }
 
-    };
+    componentDidMount() {
 
-    handleToggle = () => {
         this.setState({
-            isOpen: !this.state.isOpen
+            items: [
+                {name: "dashboard", label: "Dashboard"},
+                {
+                    name: "user",
+                    label: "User",
+                    items: [
+                        {name: "users", label: "Users"},
+                        {name: "create-user", label: "Create User"},
+                    ]
+                },
+            ]
+        });
+
+        this.state.items.map((item) => {
+            this.setState({[item.name]: false});
+        });
+    }
+
+
+    handleToggle = (name) => {
+        this.state.items.map((item) => {
+            if (item.name === name) {
+                this.setState({
+                    [name]: !this.state[name]
+                });
+            } else {
+                this.setState({[item.name]: false});
+            }
         });
     };
 
     render() {
-        const items = [
-            { name: "dashboard", label: "Dashboard" },
-            {
-                name: "user",
-                label: "User",
-                items: [
-                    { name: "users", label: "Users" },
-                    { name: "createUser", label: "Create User" },
-                ]
-            },
-        ];
         return (
             <Fragment>
                 <nav className="sidebar sidebar-offcanvas" id="sidebar">
                     <ul className="nav ps ps--active-y">
-                        {items.map((item, index) => (
-                           <Item key={index} item={item}/>
+                        {this.state.items.map((item) => (
+                            <li key={item.name} className="nav-item">
+                                <Link to='/' className={classNames({
+                                    "nav-link": true,
+                                    collapsed: !this.state[item.name]
+                                })} onClick={() => this.handleToggle(item.name)}>
+                                    <i className="ti-home menu-icon"></i>
+                                    <span className="menu-title">{item.label}</span>
+                                    {item.items && <i className="menu-arrow"></i>}
+                                </Link>
+                                {item.items ? (
+                                    <div className={classNames({"collapse": true, show: this.state[item.name]})}
+                                         id="editors">
+                                        <ul className="nav flex-column sub-menu">
+                                            {item.items.map((item) => (
+                                                <li key={item.name} className="nav-item">
+                                                    <Link to={item.name} className="nav-link">
+                                                        {item.label}
+                                                    </Link>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                ) : ('')}
+                            </li>
+
                         ))}
                     </ul>
                 </nav>
