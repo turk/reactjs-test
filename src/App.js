2c38754d0a2e0a24aa912ab03dc1438df72894ab
Suleyman Yilmaz <sy@suleymanyilmaz.org>
2020-02-09 02:53:51 +0200

    Import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import './App.css';
 import React, {Fragment} from 'react';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {PrivateRoute} from './components/PrivateRoute';
+import Nav from './components/common/Nav';
+import Sidebar from './components/common/Sidebar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <Fragment>
             <Router>
+                <Nav/>
+                <div className="main-container page-body-wrapper">
+                    <Sidebar />
+                    <div className="main-panel">
+                        <div className="content-wrapper">
+                            <Switch>
+                                <PrivateRoute path='/product'>
+                                    Product Page
+                                </PrivateRoute>
 
-              <Link to="/">Home </Link>
-              <Link to="/product">Product </Link>
-              <Link to="/login">Login </Link>
-                <Switch>
-                    <PrivateRoute path='/product'>
-                        Product Page
-                    </PrivateRoute>
+                                <PrivateRoute path='/users'>
+                                    Users List Page
+                                </PrivateRoute>
 
-                    <Route exact path='/'>
-                        Home Page
-                    </Route>
+                                <Route exact path='/'>
+                                    Home Page
+                                </Route>
 
-                    <Route exact path='/login'>
-                        Login Page
-                    </Route>
-                </Switch>
+                                <Route exact path='/login'>
+                                    Login Page
+                                </Route>
+                            </Switch>
+                        </div>
+                    </div>
+                </div>
             </Router>
         </Fragment>
     );
